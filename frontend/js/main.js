29ec5621abca0e87cc96a7e403d149928c43df82
Maksym <maksym.jagodzinski@gmail.com>
2026-04-29 12:53:13 +0200

    zmiany wizualne i bug fixy po testowaniu

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -36,6 +36,8 @@ const obserwator = new IntersectionObserver(function(entries) {
   entries.forEach(function(entry) {
     if (entry.isIntersecting) {
       entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible');
     }
   });
 }, { threshold: 0.1 });
